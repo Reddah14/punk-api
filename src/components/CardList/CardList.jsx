7f3c10121b5734096d2f63c31ffde31b41f523c0
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-19 19:07:00 +0200

    creating card list comp to hold the cards info

---
+++ src/components/CardList/CardList.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./CardList.scss";
+
+import Card from "../Card/Card";
+
+const CardList = () => {
+  return (
+    <div>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+};
+
+export default CardList;
