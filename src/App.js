1df26199bfa544087645542a439578cfeedb7e53
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-19 16:47:38 +0200

    adding checkboxes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 
 import { InputText } from "primereact/inputtext";
 import { Sidebar } from "primereact/sidebar";
+import { Checkbox } from "primereact/checkbox";
 
 import logo from "./assets/images/logo-small.jpg";
 
@@ -13,12 +14,28 @@ import "primeflex/primeflex.css";
 
 const App = () => {
   const [inputValue, setInputValue] = useState("");
+  const [checkedHighABV, setCheckedHighABV] = useState(false);
+  const [checkedClassicRange, setCheckedClassicRange] = useState(false);
+  const [checkedAcidic, setCheckedAcidic] = useState(false);
+
+  const handleCbHighABV = () => {
+    setCheckedHighABV(!checkedHighABV);
+    console.log("cb high abv ticked");
+  };
+
+  const handleCbClassicRange = () => {
+    setCheckedClassicRange(!checkedClassicRange);
+    console.log("cb classic range ticked");
+  };
+
+  const handleCbAcidicRange = () => {
+    setCheckedAcidic(!checkedAcidic);
+    console.log("cb acidic range ticked");
+  };
 
   return (
     <div className="App">
-
       <Sidebar visible={true} showCloseIcon={false}>
-
         <div className="sidebar__label shadow-4 p-3">
           <img src={logo} alt="logo" className="sidebar__label--logo mr-3" />
           <span className="sidebar__label--title">Brewdog Punk-API</span>
@@ -28,7 +45,7 @@ const App = () => {
           <span className=" p-input-icon-left p-float-label">
             <i className="pi pi-search" />
             <InputText
-              id="searchBeer"  
+              id="searchBeer"
               value={inputValue}
               onChange={(e) => setInputValue(e.target.value)}
             />
@@ -36,10 +53,50 @@ const App = () => {
           </span>
         </div>
 
+        <div className="text-left mt-5 ml-4">
+          <div className="p-field-checkbox">
+            <Checkbox
+              inputId="highABV"
+              value="High ABV"
+              onChange={handleCbHighABV}
+              checked={checkedHighABV}
+            />
+            <label className="ml-5" htmlFor="highABV">
+              High ABV
+            </label>
+          </div>
+        </div>
+
+        <div className="text-left mt-3 ml-4">
+          <div className="p-field-checkbox">
+            <Checkbox
+              inputId="classicRange"
+              value="Classic Range"
+              onChange={handleCbClassicRange}
+              checked={checkedClassicRange}
+            />
+            <label className="ml-5" htmlFor="classicRange">
+              Classic Range
+            </label>
+          </div>
+        </div>
+
+        <div className="text-left mt-3 ml-4">
+          <div className="p-field-checkbox">
+            <Checkbox
+              inputId="acidicRange"
+              value="Acidic Range"
+              onChange={handleCbAcidicRange}
+              checked={checkedAcidic}
+            />
+            <label className="ml-5" htmlFor="acidicRange">
+              Acidic Range
+            </label>
+          </div>
+        </div>
       </Sidebar>
 
       {inputValue}
-      
     </div>
   );
 };
