20a235a1acd796687d299769eb50ac01c40288ee
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-19 14:45:50 +0200

    adding search bar and able to catch user input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
-//import { InputText } from 'primereact/inputtext';
+import { InputText } from "primereact/inputtext";
 import { Sidebar } from "primereact/sidebar";
 
 import logo from "./assets/images/logo-small.jpg";
@@ -12,14 +12,34 @@ import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
 
 const App = () => {
+  const [inputValue, setInputValue] = useState("");
+
   return (
     <div className="App">
+
       <Sidebar visible={true} showCloseIcon={false}>
+
         <div className="sidebar__label shadow-4 p-3">
           <img src={logo} alt="logo" className="sidebar__label--logo mr-3" />
           <span className="sidebar__label--title">Brewdog Punk-API</span>
         </div>
+
+        <div className="p-fluid mt-6 m-3">
+          <span className=" p-input-icon-left p-float-label">
+            <i className="pi pi-search" />
+            <InputText
+              id="searchBeer"  
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
+            <label htmlFor="searchBeer">Search Your Beer</label>
+          </span>
+        </div>
+
       </Sidebar>
+
+      {inputValue}
+      
     </div>
   );
 };
