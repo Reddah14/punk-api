4fc7d1a3df5efdac24245777aded4a62bcdcd07a
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-19 14:23:19 +0200

    adding sidebar drawer

--- src/App.js
+++ src/App.js
@@ -1,34 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 //import { InputText } from 'primereact/inputtext';
-import logo from './assets/images/logo-small.jpg';
+import { Sidebar } from "primereact/sidebar";
 
-import 'primereact/resources/themes/md-light-indigo/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
+import logo from "./assets/images/logo-small.jpg";
+
+import "primereact/resources/themes/md-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 const App = () => {
-
   return (
-    <div className="App">  
-
-      <div className="topbar shadow-4 p-3">
-        <img src={logo} alt="logo" className="topbar-logo mr-3" />
-          <span className="topbar-title">Brewdog Punk-API</span>
-        <img src={logo} alt="logo" className="topbar-logo ml-3" />
-      </div>
-
-{/*       <div>
-        <InputText />
-      </div> */}
-
-{/*       <Sidebar visible={activeSidebar}>
-        sidebar content
-      </Sidebar> */}
+    <div className="App">
+      <Sidebar visible={true} showCloseIcon={false}>
+        <div className="sidebar__label shadow-4 p-3">
+          <img src={logo} alt="logo" className="sidebar__label--logo mr-3" />
+          <span className="sidebar__label--title">Brewdog Punk-API</span>
+        </div>
+      </Sidebar>
     </div>
   );
-}
+};
 
 export default App;
