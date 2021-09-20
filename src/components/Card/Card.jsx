b55967228b244dbbc3db38d3e9cd0329623f2b98
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-20 23:00:40 +0200

    card small style fix

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,11 +5,10 @@ import './Card.scss'
 const Card = (props) => {
   const { beerImg, beerName, beerDescription } = props;
 
-  const formattedDescription = beerDescription.substring(0, 125);
-
+  const formattedDescription = beerDescription.substring(0, 80) + '...';
   return (
     <div>
-      <div className="p-card shadow-5">
+      <div className="p-card shadow-5 p-4">
         <img src={beerImg} alt="beer" className="beer-image" />
         <h5>{beerName}</h5>
         <p>{formattedDescription}</p>
