59f8ebd8db22e1a20178ec4418972b30845cf4d0
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-20 15:18:41 +0200

    move sidebar into Home in order to apply filter logic

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Home.scss";
 
+import { Sidebar } from "primereact/sidebar";
+
+import beerData from "../data/beers-data";
+//https://api.punkapi.com/v2/
+
+import logo from "../assets/images/logo-small.jpg";
+import Searchbar from "../components/Searchbar/Searchbar";
+import FilterList from "../components/FilterList/FilterList";
 import CardList from "../components/CardList/CardList";
 
 const Home = () => {
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleInput = (event) => {
+    const cleanInput = event.target.value.toLowerCase();
+    setSearchTerm(cleanInput);
+    console.log(event.target.value);
+  };
+
+  const filteredBeers = beerData.filter((beer) => {
+    const beerNameToLowerC = beer.name.toLowerCase();
+    const searchTermToLowerC = searchTerm.toLowerCase();
+
+    return beerNameToLowerC.includes(searchTermToLowerC);
+  })
+
   return (
     <div className="home">
-      <CardList />
+      {/* <LeftDrawer /> */}
+
+
+      <Sidebar visible={true} showCloseIcon={false}>
+        <div className="sidebar__label shadow-4 p-3">
+          <img src={logo} alt="logo" className="sidebar__label--logo mr-3" />
+          <span className="sidebar__label--title">Brewdog Punk-API</span>
+        </div>
+        <Searchbar
+          label="Search Your Beer"
+          beerToSearch={searchTerm}
+          handleInput={handleInput}
+        />
+        <FilterList />
+      </Sidebar>
+
+
+      <CardList beerData={filteredBeers} />
     </div>
   );
 };
