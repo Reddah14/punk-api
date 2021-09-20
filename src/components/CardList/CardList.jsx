17860fbac5c45a824b81ce0b1d95f4e02e84b6c6
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-20 13:19:31 +0200

    importing mock data and mapping it

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -2,14 +2,20 @@ import React from "react";
 import "./CardList.scss";
 
 import Card from "../Card/Card";
+import beerData from "../../data/beers-data";
+
+
 
 const CardList = () => {
+  console.log(beerData);
+
+  const beers = beerData.map((beer) => {
+    return <Card beerId={beer.id} beerName={beer.name} beerImg={beer.image_url} firstBrewed={beer.first_brewed} beerPh={beer.ph} beerAvb={beer.abv} beerDescription={beer.description} /> 
+  });
+
   return (
-    <div>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+    <div className="main-layout flex flex-wrap mt-2 mr-2 mb-2" >
+      {beers}
     </div>
   );
 };
