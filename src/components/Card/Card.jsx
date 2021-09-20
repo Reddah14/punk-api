02955ff10570f8fb17ee41f40663d2c8ea0376c9
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-20 12:57:59 +0200

    first approach to card comp

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
 import './Card.scss'
+import pic from '../../assets/images/bottle.png'
 
 const Card = () => {
   return (
     <div>
-      single card
+      <div className="p-card shadow-5">
+        <img src={pic} alt="bottle" className="beer-image" />
+        <h5>Beer Name</h5>
+        <p>Beer Description... Lorem ipsum dolor sit amet.Beer Description... Lorem ipsum dolor sit amet.</p>
+      </div>
     </div>
   )
 }
 
-export default Card
+export default Card;
