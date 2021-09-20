f126a136d667179ab18e3d330f90936470b8c99c
Salvador Martinez <cartoonrocket9@gmail.com>
2021-09-20 15:17:50 +0200

    add key to avoid error on console & pass data to comp as props instead

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -2,15 +2,13 @@ import React from "react";
 import "./CardList.scss";
 
 import Card from "../Card/Card";
-import beerData from "../../data/beers-data";
 
-
-
-const CardList = () => {
+const CardList = (props) => {
+  const { beerData } = props;
   console.log(beerData);
 
   const beers = beerData.map((beer) => {
-    return <Card beerId={beer.id} beerName={beer.name} beerImg={beer.image_url} firstBrewed={beer.first_brewed} beerPh={beer.ph} beerAvb={beer.abv} beerDescription={beer.description} /> 
+    return <Card key={beer.id + 1} beerId={beer.id} beerName={beer.name} beerImg={beer.image_url} firstBrewed={beer.first_brewed} beerPh={beer.ph} beerAvb={beer.abv} beerDescription={beer.description} /> 
   });
 
   return (
